4a153572bf2688f0cd135d7c0ad3a8597b110468
Ruslan Batkaev <ruslan.batkaev@muranosoft.com>
2017-10-19 12:37:07 +0300

    project was added

---
+++ StackTracePerformance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackTracePerformance
+{
+    public class Program
+    {
+	    public static void Main()
+	    {
+		    for (int i = 0; i < 10000; i++)
+		    {
+			    new StackTrace(true).GetFrames();
+		    }
+	    }
+    }
+}
